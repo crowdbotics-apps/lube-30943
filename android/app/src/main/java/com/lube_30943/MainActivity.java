4469ffaa81af1b9155c8259c30a006081ad73967
Crowdbotics <team@crowdbotics.com>
2021-09-28 23:40:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lube_30943/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lube_30943;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "lube_30943";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lube_30943",
+  url: "https://lube_30943.botics.co",
+  api: "https://lube_30943.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
